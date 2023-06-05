d58445b94fae7c62e16526856549cde1047b8816
karan gupta <gkaran2112@gmail.com>
2023-06-06 01:49:11 +0530

    loops

---
+++ whileloop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int i=50;
+  while(i<=100)
+  {
+    cout<<i<<endl;
+    i++;
+  }
+  return 0;
+}
+
+// variable declaration;
+// while(condition)
+// {
+//     cout<<...............
+    
+// }
