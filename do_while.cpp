87af986d78e38a69d450ae1f43a0ecaf006e31a0
karan gupta <gkaran2112@gmail.com>
2023-06-12 22:15:06 +0530

    do while

---
+++ do_while.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 10;
+    do
+    {
+        cout << i << endl;
+        i++;
+    } while (i <= 50);
+    return 0;
+}
+
+/*variable declartion;
+do{
+    cout<<..............................:
+
+}
+while(condtion);
+
+*/
---
+++ tableusingdowhile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    cout << "ENTER A NUMBER :";
+    cin >> i;
+    int j = 1;
+    do
+    {
+        cout << i <<" "<< "*" <<" "<< j <<" "<<"="<<" " << i * j << endl;
+        j++;
+    } while (j <= 10);
+    return 0;
+}
